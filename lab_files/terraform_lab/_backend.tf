621240b44dc56ea7810cb604ab0051b742fdf923
ravensp93 <tbchin93@gmail.com>
2023-10-18 07:28:38 +0000

    updated for finance deployment

--- lab_files/terraform_lab/_backend.tf
+++ lab_files/terraform_lab/_backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        key = "finance/front-end-systems/terraform.tfstate"
-        region = "us-east-1"
-        bucket = "tf-state-storage-202202"
-        dynamodb_table = "for-terraform-state-lock"
-        encrypt = true
-    }
+  backend "s3" {
+    key            = "finance/front-end-systems/terraform.tfstate"
+    region         = "us-east-1"
+    bucket         = "terraform-state-20231018"
+    dynamodb_table = "terraform_lock_table"
+    encrypt        = true
+  }
 }
